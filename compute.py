29aa4322d3b36cef97b1c222df1ebf4898bc578a
Nolan Greenup <38223198+nrgreenup@users.noreply.github.com>
2024-09-27 10:45:49 -0400

    add annuity due. refactor to read more like a normal geometric sum

--- compute.py
+++ compute.py
@@ -1,35 +1,52 @@
 import numpy as np
+from typing import Literal
 
-def toEquivalentRate(annualRate: float, newCompounding: int, oldCompunding: int = 1):
+def toEquivalentRate(annualRate: float, newCompounding: int, oldCompunding: int = 1) -> float:
     """
-    What: 
-    Convert nominal interest rates of a given compounding period to an equivalent interest rate given a different compounding period.
+    Convert nominal interest rates between different compounding periods.
+    Allows projection of investments with different contribution schedules by converting them to equivalent interest rates.
+
+        See: https://www.calculatorsoup.com/calculators/financial/equivalent-interest-rate-calculator.php
+    """
     
-    Why:
-    We normally think of returns in annual terms (e.g., the S&P500 returns about 10.5% annually, on average).
-    But, people invest at differing schedules (monthly, quarterly, semi-annually, annually... or commonly a combination of them).
-    So, we need to be able to move between effective interest rates based on contribution frequency.
-
-    Ref: 
-    See for explanation and formula: https://www.calculatorsoup.com/calculators/financial/equivalent-interest-rate-calculator.php
-    """
     return newCompounding * ((1 + annualRate / oldCompunding) ** (oldCompunding / newCompounding) - 1)
 
-def averageReturnPerPeriod(rate: float, contributionsPerYear: int):
+def averageReturnPerPeriod(rate: float, contributionsPerYear: int) -> float:
     """
-    What:
     Average the returns over the number of contributions per year. 
-
-    Why:
-    If we return 6% per year, we need to break that over the contribution frequency. If we contribute 12 times a year, we then assume .5% for each contribution per month.
-    Assumes uniformity month-over-month, which of course isn't the case, but is a decent approximator absent getting into much more complex approaches.
+    e.g., 10% nominal return annually with quarterly contributions is 2.5% nominal per quarter.
     """
+
     return 1 + rate / contributionsPerYear
 
-def toFutureValueOfContribution(annualRate: float, amount: int, contributionsPerYear: int, durationYears: int):
-    growthFactor = averageReturnPerPeriod(toEquivalentRate(annualRate, contributionsPerYear, 1), contributionsPerYear)
-    totalContributions = contributionsPerYear * durationYears
-    return amount * (1 - pow(growthFactor, totalContributions)) / (1 - growthFactor)
+def sumGeometricSeries(amount: float, commonRatio: float, periods: int) -> float:
+    """
+    Typical sum of a geometric series.
+    """
+
+    ## need to check this as geometric series is undefined for a commonRatio of 1, which here is no growth
+    if (commonRatio == 1):
+        return amount * periods
+    
+    return amount * (1 - commonRatio ** periods) / (1 - commonRatio)
+
+def toFutureValueOfContribution(annualRate: float, contributionAmount: int, contributionsPerYear: int, durationYears: int, timing: Literal["start, end"] = "end") -> float:
+    """
+    Compute the future value of a defined contribution scheme (growth rate, amount, duration, timing of payments within period).
+    """
+
+    ratePerPeriod = toEquivalentRate(annualRate, contributionsPerYear, 1) / contributionsPerYear
+    totalPeriods = contributionsPerYear * durationYears
+    finalValue = sumGeometricSeries(contributionAmount, 1 + ratePerPeriod, totalPeriods)
+
+    ## less commonly, an annuity due (i.e., you make contributions at the start of each period)
+    ## i.e., you get extra investment period
+    if timing == "start":
+        return finalValue * (1 + ratePerPeriod)
+
+    return finalValue
+
 
 print(toEquivalentRate(.10, 12))
 print(toFutureValueOfContribution(.10, 1000, 12, 5))
+print(toFutureValueOfContribution(.10, 1000, 12, 5, "start"))
