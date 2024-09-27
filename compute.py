7844fcbd2f3d727f677ad331b3dec6f781ad4ef6
Nolan Greenup <38223198+nrgreenup@users.noreply.github.com>
2024-09-27 14:13:49 -0400

    dynamic contribution freuqencies, WIP need to refactor for clarity

--- compute.py
+++ compute.py
@@ -19,7 +19,7 @@ def averageReturnPerPeriod(rate: float, contributionsPerYear: int) -> float:
 
     return 1 + rate / contributionsPerYear
 
-def cumulativeGeometricSeries(amount: float, commonRatio: float, periods: int) -> float:
+def cumulativeGeometricSeries(amount: float, commonRatio: float, periods: float) -> float:
     """
     Typical sum of a geometric series.
     """
@@ -30,25 +30,52 @@ def cumulativeGeometricSeries(amount: float, commonRatio: float, periods: int) -
         
         return amount * (1 - commonRatio ** p) / (1 - commonRatio)
 
-    return np.array([sumGeometricSeries(p) for p in range(1, periods + 1)])
+    # return np.array([sumGeometricSeries(p) for p in range(1, periods + 1)])
+    
+    return np.array([sumGeometricSeries(p) for p in periods])
 
-def toFutureValueOfContribution(annualRate: float, contributionAmount: int, contributionsPerYear: int, durationYears: int, timing: Literal["start, end"] = "end") -> float:
+
+# def futureValueOf(contributionsPerYear: int, durationYears: float):
+#     def withValues(annualRate: float, )
+
+## typing constructs
+ContributionFrequency = {
+    "daily": 365.242,
+    "weekly": 52.1775,
+    "biweekly": 26.08875,
+    "monthly": 12,
+    "quarterly": 4,
+    "biannually": 2,
+    "annually": 1
+}
+
+## TODO: pylance gets mad at this because you can't use a variable in a type expression
+AllowedContributionFrequencies = Literal[tuple(ContributionFrequency.keys())]
+
+ContributionTiming = Literal["start", "end"]
+
+def toFutureValueOfContribution(annualRate: float, contributionAmount: int, frequency: AllowedContributionFrequencies, durationYears: float, timing: ContributionTiming = "end") -> float:
     """
     Compute the future value of a defined contribution scheme (growth rate, amount, duration, timing of payments within period).
     """
-
+    contributionsPerYear = ContributionFrequency[frequency]
     ratePerPeriod = toEquivalentRate(annualRate, contributionsPerYear, 1) / contributionsPerYear
     totalPeriods = contributionsPerYear * durationYears
-    finalValue = cumulativeGeometricSeries(contributionAmount, 1 + ratePerPeriod, totalPeriods)
+    incrementor = 1 / (12 / contributionsPerYear)
+    periodRange = np.linspace(incrementor, totalPeriods, int(durationYears * 12))
+    finalValues = cumulativeGeometricSeries(contributionAmount, 1 + ratePerPeriod, periodRange)
 
     ## less commonly, an annuity due (i.e., you make contributions at the start of each period)
     ## i.e., you get extra investment period
     if timing == "start":
-        return np.array([x * (1 + ratePerPeriod) for x in finalValue])
+        return np.array([x * (1 + ratePerPeriod) for x in finalValues])
 
-    return finalValue
+    return finalValues
 
 
 print(toEquivalentRate(.10, 12))
-print(toFutureValueOfContribution(.10, 1000, 12, 5))
-print(toFutureValueOfContribution(.10, 1000, 12, 5, "start"))
+# print(toFutureValueOfContribution(.10, 1000, 12, 5))
+# print(toFutureValueOfContribution(.10, 1000, 12, 5, "start"))
+print(toFutureValueOfContribution(.10, 1000, 'weekly', 5/12))
+print(toFutureValueOfContribution(.065, 1006, 'quarterly', 7/12))
+
